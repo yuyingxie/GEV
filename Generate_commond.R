7e53e75ed4854846f24c410ee34f2286efa75bba
yuyingxie <yuyingxie@gmail.com>
2018-07-11 16:58:09 -0400

    a

--- Generate_commond.R
+++ Generate_commond.R
@@ -5,10 +5,10 @@ for (i in 1:10){
 		for(p in c(500)){
 				for(n in c(300)){
 							write(paste("R CMD BATCH --no-save --no-restore '--args case.id=", 
-											i, " Type=\"",  Type, " p=", p, " n=", n,  "' GEV_study.R temp/GEV.output._", Type, 
+											i, " Type=\"",  Type, "\"", " p=", p, " n=", n,  "' GEV_study.R temp/GEV.output._", Type, 
 											"_",  "p", p, "_n",n, "_id", i, 
 											".txt", sep=""), file = "command_list", append = TRUE)
 						}
 					}
 				}
-			}
+}
