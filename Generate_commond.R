631f42bd2df6d6e478aadbbda2152ceebf53b692
yuyingxie <yuyingxie@gmail.com>
2018-07-13 16:19:57 -0400

    a

--- Generate_commond.R
+++ Generate_commond.R
@@ -1,9 +1,9 @@
 
 unlink('command_list')
-for (i in 1:10){
+for (i in 11:30){
 	for(Type in c("I")){
-		for(p in c(500)){
-				for(n in c(2000)){
+		for(p in c(50)){
+				for(n in c(300)){
 							write(paste("R CMD BATCH --no-save --no-restore '--args case.id=", 
 											i, " Type=\"",  Type, "\"", " p=", p, " n=", n,  "' GEV_study.R temp/GEV.output._", Type, 
 											"_",  "p", p, "_n",n, "_id", i, 
