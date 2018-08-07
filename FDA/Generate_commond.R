5259eb6fe51b5c68c4053abdeeee5f5ca896d993
yuyingxie <yuyingxie@gmail.com>
2018-08-07 17:46:54 -0400

    a

--- FDA/Generate_commond.R
+++ FDA/Generate_commond.R
@@ -5,7 +5,7 @@ for (i in 1:10){
 		for(p in c(500)){
 				for(n in c(150)){
 							write(paste("R CMD BATCH --no-save --no-restore '--args case.id=", 
-											i, " Type=\"",  Type, "\"", " p=", p, " n=", n,  "' GEV_study.R temp/GEV.output._", Type, 
+											i, " Type=\"",  Type, "\"", " p=", p, " n=", n,  "' FDA_study.R temp/GEV.output._", Type, 
 											"_",  "p", p, "_n",n, "_id", i, 
 											".txt  &", sep=""), file = "command_list", append = TRUE)
 						}
